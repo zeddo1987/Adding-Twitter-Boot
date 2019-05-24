5e59bceb902ac8a874bc69e6e55b473f2f9598a0
GBTC406013UR <GBTC406013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-24 11:30:20 -0400

    AddingBootStramp

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class HomeController {
+    @RequestMapping
+    public String index(){
+        return "index";
+    }
+}
